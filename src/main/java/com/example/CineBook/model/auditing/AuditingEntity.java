4e3a28546e59be3ffe2f2ea038fe27e1e554cf31
vlongle29 <longvis2004@gmail.com>
2025-11-25 21:15:24 +0700

    refactor(comment): update

--- src/main/java/com/example/CineBook/model/auditing/AuditingEntity.java
+++ src/main/java/com/example/CineBook/model/auditing/AuditingEntity.java
@@ -36,7 +36,26 @@ public abstract class AuditingEntity implements SoftDeletable {
         return this.isDelete;
     }
 
-
 }
 
 /// Kiến thức học được
+/**
+ * @MappedSuperclass dùng để khai báo rằng class này không phải là entity, nhưng các entity con kế thừa
+ * nó sẽ thừa hưởng toàn bộ field và mapping.
+ * Nói đơn giản:
+ *
+ * Class cha không tạo bảng trong DB.
+ * Các class con có @Entity sẽ nhận toàn bộ thuộc tính + mapping của class cha.
+ * Thường dùng để tạo base entity, như createTime, updateTime, id...
+ *
+ * @EntityListeners(AuditingEntityListener.class)
+ * Annotation này nói với JPA rằng:
+ * --> “Hãy sử dụng class AuditingEntityListener để lắng nghe các sự kiện lifecycle như:
+ * @PrePersist, @PostPersist, @PreUpdate, @PreRemove, ...”
+ * Nó cho phép bạn tự động:
+ * Gắn createTime khi entity được insert
+ * Gắn updateTime khi entity được update
+ * Tự động set createBy, updateBy
+ * Tự xử lý khi entity bị soft-delete
+ *
+ */
