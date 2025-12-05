2a4f0d97cfd2f4d1b1bb51dcef61bc5552d680e5
vlongle29 <longvis2004@gmail.com>
2025-12-05 18:41:20 +0700

    fix(Customer): Fix name

--- src/main/java/com/example/CineBook/repository/custom/CustomerRepositoryCustom.java
+++ src/main/java/com/example/CineBook/repository/custom/CustomerRepositoryCustom.java
@@ -3,7 +3,9 @@ package com.example.CineBook.repository.custom;
 import com.example.CineBook.dto.customer.CustomerSearchDTO;
 import com.example.CineBook.model.Customer;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
 
 public interface CustomerRepositoryCustom {
-    Page<Customer> findAllWithFilters(CustomerSearchDTO searchDTO);
+    Page<Customer> searchWithFilters(CustomerSearchDTO searchDTO, Pageable pageable);
 }
