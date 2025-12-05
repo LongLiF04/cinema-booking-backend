c9b56dfd7336f24a440fcb4b1070c03877fc66db
vlongle29 <longvis2004@gmail.com>
2025-12-05 18:43:48 +0700

    fix(Genre): Add softDeleteById

--- src/main/java/com/example/CineBook/repository/irepository/GenreRepository.java
+++ src/main/java/com/example/CineBook/repository/irepository/GenreRepository.java
@@ -1,17 +1,22 @@
 package com.example.CineBook.repository.irepository;
 
-import com.example.CineBook.dto.genre.GenreSearchDTO;
 import com.example.CineBook.model.Genre;
-import com.example.CineBook.repository.base.BaseRepositoryCustom;
 import com.example.CineBook.repository.custom.GenreRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface GenreRepository extends BaseRepositoryCustom<Genre, GenreSearchDTO>, JpaRepository<Genre, UUID>, GenreRepositoryCustom{
+public interface GenreRepository extends JpaRepository<Genre, UUID>, GenreRepositoryCustom {
     boolean existsByName(String name);
     Optional<Genre> findByName(String name);
+    
+    @Modifying
+    @Query("UPDATE Genre g SET g.isDelete = true, g.deleteTime = CURRENT_TIMESTAMP WHERE g.id = :id")
+    void softDeleteById(@Param("id") UUID id);
 }
