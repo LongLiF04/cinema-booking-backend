1aa214486a7d526ead1d51f8a91b5698e1c09326
vlongle29 <longvis2004@gmail.com>
2025-12-07 00:16:54 +0700

    fix(Showtime): Add UpdateShowtimeRequest

---
+++ src/main/java/com/example/CineBook/dto/showtime/UpdateShowtimeRequest.java
@@ -0,0 +1,29 @@
+package com.example.CineBook.dto.showtime;
+
+import com.example.CineBook.common.constant.ShowtimeStatus;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateShowtimeRequest {
+    
+    private UUID movieId;
+    private UUID roomId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    
+    @Positive(message = "Price must be greater than 0")
+    private BigDecimal price;
+    
+    private ShowtimeStatus status;
+}
